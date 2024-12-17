d88c90f2f70f7a674435f5bfb052ab55278e64f7
Maxim Zyuryaev <zrv.maxim@gmail.com>
2024-12-17 11:04:59 +0200

    added a color pallet, designed login|register|home pages and linked activities to them

---
+++ ShopMiniProj/Activities/LoginActivity.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace ShopMiniProj.Activities
+{
+	[Activity (Label = "@string/loginGreeting", Theme = "@style/AppTheme", MainLauncher =true)]			
+	public class LoginActivity : AppCompatActivity
+    {
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your application here
+			SetContentView(Resource.Layout.login);
+			var register = FindViewById<Button>(Resource.Id.button_register);
+			var intent = new Intent(this, typeof(RegisterActivity));
+			register.Click += (sender, e) => StartActivity(intent);
+		}
+	}
+}
+
---
+++ ShopMiniProj/Activities/RegisterActivity.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace ShopMiniProj.Activities
+{
+	[Activity (Label = "RegisterActivity")]			
+	public class RegisterActivity : AppCompatActivity
+    {
+		Button cencel_btn;
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your application here
+			SetContentView(Resource.Layout.register);
+
+			Init();
+		}
+
+        private void Init()
+        {
+			cencel_btn = FindViewById<Button>(Resource.Id.button_cencel);
+			cencel_btn.Click += (sender, e) => Finish();
+        }
+    }
+}
+
